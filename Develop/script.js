46d3f39636d6f28848bc4989253f45af0e58d6c7
David Sklar <davidhsklar@gmail.com>
2022-03-06 21:03:09 -0500

    code cleanup

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,10 @@
+// Application code
 
 var Characters = {
   upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lower: "abcdefghijklmnopqrstuvwxyz",
   number: "0123456789",
   sp: "!\"#$%&'()*+",
-  reset: function () {
-    this.upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    this.lower = "abcdefghijklmnopqrstuvwxyz",
-    this.number = "0123456789",
-    this.sp = "!\"#$%&'()*+" 
-  }
 }
 
 
@@ -17,19 +12,20 @@ var Characters = {
 
 var generatePassword = function() {
   var Confirm = window.confirm(
-    "Welcome to the Password Generator.  Please press OK to get started. "
+    "Hello and welcome to the random password generator. This site will generate a random password based on user defined rules.  Please press OK to get started. "
     );
-    //starts prompts on true answer
+
+    // if they press ok, let's start
 
     if (Confirm) {
 
-   //length of password
+   // length of password
 
    var passLengthFinal = function () {
     var passLength = (window.prompt("Please specify the number of characters preferred for your password between 8 and 128"));
 
-    if (isNaN(passLength) || passLength < 8 || passLength > 128) {
-      alert("Please enter only a NUMBER between 8 and 128");
+    if (passLength < 8 || passLength > 128) {
+      alert("Please enter a number between 8 and 128");
       return passLengthFinal();
     } else {
       return passLength;
@@ -37,61 +33,70 @@ var generatePassword = function() {
     
   }
   
-  //storing password length in variable to be called back
+  // converting password length into function to be called back later
+
   var passLengthActual = passLengthFinal();
   window.alert(" You have selected " + passLengthActual + " characters! ")
   
 
-//variables for password criteria
+// password criteria set-up.  set the variables first
 
-var confirmUppercase = true;
-var confirmLowercase = true;
-var confirmNumbers = true;
-var confirmSpecial = true;
+var selectedUppercase = true;
+var selectedLowercase = true;
+var selectedNumbers = true;
+var selectedSpecial = true;
 
 // prompts for password criteria
-while (confirmUppercase === true && confirmLowercase === true && confirmNumbers === true && confirmSpecial === true) {
+while (selectedUppercase === true && selectedLowercase === true && selectedNumbers === true && selectedSpecial === true) {
 window.alert(" Please select at least one character type.");
-var confirmUppercase = window.confirm(" Would you like to include uppercase characters? ");
-var confirmLowercase = window.confirm(" Would you like to include lowercase characters? ");
-var confirmNumbers = window.confirm(" Would you like to include numbers? ");
-var confirmSpecial = window.confirm(" Would you like to include special characters? ");
+
+// upper case
+var selectedUppercase = window.confirm(" Would you like to include uppercase characters? ");
+// lower case
+var selectedLowercase = window.confirm(" Would you like to include lowercase characters? ");
+// numbers
+var selectedNumbers = window.confirm(" Would you like to include numbers? ");
+// special characters
+var selectedSpecial = window.confirm(" Would you like to include special characters? ");
 break;
 }
 
-// character selection 
-if (confirmUppercase) {
+// character selection from criteria prompts 
+
+if (selectedUppercase) {
 Characters.upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 } else {
   Characters.upper = "";
 }
-if (confirmLowercase) {
+if (selectedLowercase) {
 Characters.lower = "abcdefghijklmnopqrstuvwxyz";
 } else {
   Characters.lower = "";
 }
-if (confirmNumbers) {
+if (selectedNumbers) {
 Characters.number = "0123456789";
 } else {
   Characters.number = "";
 }
-if (confirmSpecial) {
+if (selectedSpecial) {
 Characters.sp = "!\"#$%&'()*+";
 } else {
   Characters.sp = "";
 }
 
-// setting availble character list for generation
-var characterList = Characters.upper + Characters.lower + Characters.number + Characters.sp;
-console.log(characterList)
+// defining the selected characters so they can be called in the random generation loop
 
-//character generation code
+var selectedCharacters = Characters.upper + Characters.lower + Characters.number + Characters.sp;
+console.log(selectedCharacters)
+
+// character generation loop
 
 var result = "";
     for (var i = 0; i <= passLengthActual - 1; i++) {
-      result += characterList.charAt(Math.floor(Math.random() * characterList.length));
+      result += selectedCharacters.charAt(Math.floor(Math.random() * selectedCharacters.length));
     }
 
+    alert(result);
     console.log(result);
 
   var finalPassword = result;
